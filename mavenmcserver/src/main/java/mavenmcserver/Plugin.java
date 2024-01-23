28dc128096b38ea6fc6eb61694e25484921740b2
levischnorr <levischnorr@web.de>
2024-01-23 15:23:16 +0100

    Register command executor and send message to CommandSender

--- mavenmcserver/src/main/java/mavenmcserver/Plugin.java
+++ mavenmcserver/src/main/java/mavenmcserver/Plugin.java
@@ -2,11 +2,14 @@ package mavenmcserver;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import mavenmcserver.command.CommandTicTacToe;
+
 public class Plugin extends JavaPlugin {
 
 	@Override
 	public void onLoad() {
 		this.getLogger().info("TicTacToe loaded!");
+		this.getCommand(CommandTicTacToe.commandName).setExecutor(new CommandTicTacToe());
 	}
 	
 	@Override
--- mavenmcserver/src/main/java/mavenmcserver/command/CommandTicTacToe.java
+++ mavenmcserver/src/main/java/mavenmcserver/command/CommandTicTacToe.java
@@ -5,9 +5,13 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class CommandTicTacToe implements CommandExecutor {
+	
+	public static String commandName = "tictactoe";
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		sender.sendMessage("You just executed /tictactoe");
 		
 		return true;
 	}
