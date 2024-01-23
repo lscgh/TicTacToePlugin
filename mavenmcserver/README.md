259a078330d3e4c0d0fe2620ee8f405886d23c20
levischnorr <levischnorr@web.de>
2024-01-23 13:54:48 +0100

    Add plugin structure

---
+++ mavenmcserver/README.md
@@ -0,0 +1,3 @@
+# TicTacToePlugin
+
+TODO
---
+++ mavenmcserver/src/main/java/mavenmcserver/Plugin.java
@@ -0,0 +1,22 @@
+package mavenmcserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+		this.getLogger().info("TicTacToe loaded!");
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
