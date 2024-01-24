75d6a8d0db40e87f09b88acbc839937a65163fe2
levischnorr <levischnorr@web.de>
2024-01-24 10:07:43 +0100

    Fixes

--- mavenmcserver/src/main/java/mavenmcserver/command/CommandTicTacToe.java
+++ mavenmcserver/src/main/java/mavenmcserver/command/CommandTicTacToe.java
@@ -11,11 +11,11 @@ public class CommandTicTacToe implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		
-		if(args.length > 1) {
+		if(args.length > 0) {
 			sender.sendMessage("You just executed /tictactoe correctly");
 		}
 		
-		return args.length > 1;
+		return args.length > 0;
 	}
 
 	
