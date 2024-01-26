992a7fe6a79069ee5ae5bad732dab3cc41e6841a
levischnorr <levischnorr@web.de>
2024-01-26 12:53:58 +0100

    Extend test for GameConfig

--- mavenmcserver/src/test/java/mavenmcserver/game/GameConfigTest.java
+++ mavenmcserver/src/test/java/mavenmcserver/game/GameConfigTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class GameConfigTest {
 
 	@Test
-	public void testValidateTest() {
+	public void testValidate() {
 		GameConfig config = new GameConfig(null, null, new Vector3i(0, 0, 0), 2);
 		List<String> errors = config.validate();
 		assertTrue(errors.contains("Couldn't add you to the game. Please retry!"));
@@ -18,6 +18,11 @@ public class GameConfigTest {
 		assertTrue(errors.contains("No dimension of the game can be smaller than 1. The smallest possible game is (" + GameConfig.minFlatSize + ", " + GameConfig.minHeight + ", " + GameConfig.minFlatSize + ")."));
 		assertTrue(errors.contains("The X and Z size of the game must not be smaller than " + GameConfig.minFlatSize));
 		assertTrue(errors.contains("The required win amount must not be larger than the size's largest dimension"));
+		
+		config = new GameConfig(null, null, new Vector3i(2, 1, 2), 2);
+		errors = config.validate();
+		assertTrue(errors.contains("Couldn't add you to the game. Please retry!"));
+		assertTrue(errors.contains("Couldn't add the opponent player to the game."));
 	}
 	
 }
