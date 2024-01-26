df2a022fa7bba6313f734f8b3cc42eb279640bff
levischnorr <levischnorr@web.de>
2024-01-26 09:15:31 +0100

    Add more restrictions to GameConfig validation

--- mavenmcserver/src/main/java/mavenmcserver/game/GameConfig.java
+++ mavenmcserver/src/main/java/mavenmcserver/game/GameConfig.java
@@ -7,6 +7,10 @@ import org.bukkit.entity.Player;
 import org.joml.Vector3i;
 
 public class GameConfig {
+	
+	static int minFlatSize = 2;
+	static int minHeight = 1;
+	
 	public Player mainPlayer;
 	public Player opponentPlayer;
 	public Vector3i size;
@@ -22,6 +26,22 @@ public class GameConfig {
 	/// Returns an empty list if everything is OK.
 	public List<String> validate() {
 		ArrayList<String> errors = new ArrayList<String>();
+		
+		if(this.mainPlayer == null) {
+			errors.add("Couldn't add you to the game. Please retry!");
+		}
+		
+		if(this.opponentPlayer == null) {
+			errors.add("Couldn't add the opponent player to the game.");
+		}
+		
+		if(Math.min(this.size.x, Math.min(this.size.y, this.size.z)) <= 0) {
+			errors.add("No dimension of the game can be smaller than 1. The smallest possible game is (" + GameConfig.minFlatSize + ", " + GameConfig.minHeight + ", " + GameConfig.minFlatSize + ").");
+		}
+		
+		if(Math.min(this.size.x, this.size.z) < GameConfig.minFlatSize) {
+			errors.add("The X and Z size of the game must not be smaller than " + GameConfig.minFlatSize);
+		}
 		
 		if(this.winRequiredAmount > Math.max(this.size.x, Math.max(this.size.y, this.size.z))) {
 			errors.add("The required win amount must not be larger than the size's largest dimension");
