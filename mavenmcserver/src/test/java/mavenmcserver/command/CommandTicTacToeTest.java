5d5a6bee9c4f84f9f01e3fbace5e6e6ae309b462
levischnorr <levischnorr@web.de>
2024-01-29 12:31:20 +0100

    Extend CommandTicTacToe test

--- mavenmcserver/src/test/java/mavenmcserver/command/CommandTicTacToeTest.java
+++ mavenmcserver/src/test/java/mavenmcserver/command/CommandTicTacToeTest.java
@@ -13,10 +13,16 @@ public class CommandTicTacToeTest {
 		assertArrayEquals(new int[]{3, 1, 3, 3}, CommandTicTacToe.extractIntegerArgs(new String[]{"OpponentName", "3", "1", "3", "3"}));
 		assertArrayEquals(new int[]{0, 0, 0, 0}, CommandTicTacToe.extractIntegerArgs(new String[]{"OpponentName", "0", "0", "0", "0"}));
 		assertArrayEquals(new int[]{1, 2, 3, 4}, CommandTicTacToe.extractIntegerArgs(new String[]{"OpponentName", "1", "2", "3", "4"}));
+		assertArrayEquals(new int[]{-1, 2, -3, 4}, CommandTicTacToe.extractIntegerArgs(new String[]{"OpponentName", "-1", "2", "-3", "4"}));
 		
 		try {
 			CommandTicTacToe.extractIntegerArgs(new String[]{"OpponentName", "e", "2", "3", "4"});
 			assertTrue(false, "CommandTicTacToe.extractIntegerArgs didn't throw an exception with {'OpponentName', 'e', '2', '3', '4'}");
+		} catch(NumberFormatException e) {}
+		
+		try {
+			CommandTicTacToe.extractIntegerArgs(new String[]{"OpponentName", "10", "2", "3", "4."});
+			assertTrue(false, "CommandTicTacToe.extractIntegerArgs didn't throw an exception with {'OpponentName', '10', '2', '3', '4.'}");
 		} catch(NumberFormatException e) {}
 		
 	}
